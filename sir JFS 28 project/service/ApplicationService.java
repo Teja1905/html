cbfadc90345e3eb558162453f9a7635b950e5d24
Teja1905 <j.pranavteja@gmail.com>
2020-02-09 11:05:05 +0530

    inii

---
+++ sir JFS 28 project/service/ApplicationService.java
@@ -0,0 +1,15 @@
+package com.slokam.test.service;
+
+import com.slokam.test.entity.Application;
+import com.slokam.test.repo.ApplicationRepo;
+
+import java.util.*;
+
+public interface ApplicationService {
+	
+	public List<Application> getAll();
+	public Application getById(Integer id);
+	public Application save(Application Application);
+	public void remove(Application Application);
+
+}
---
+++ sir JFS 28 project/service/impl/ApplicationServiceImpl.java
@@ -0,0 +1,45 @@
+package com.slokam.test.service.impl;
+
+import com.slokam.test.entity.Application;
+import com.slokam.test.repo.ApplicationRepo;
+import com.slokam.test.service.ApplicationService;
+import org.springframework.stereotype.Service;
+import java.util.*;
+import org.springframework.beans.factory.annotation.Autowired;
+
+@Service
+public class ApplicationServiceImpl implements ApplicationService {
+
+      
+   
+
+	@Autowired
+	private ApplicationRepo repo;
+	
+	@Override
+	public List<Application> getAll() {
+		return repo.findAll();
+	}
+
+	@Override
+	public Application getById(Integer id) {
+		
+		Optional<Application> opt = repo.findById(id);
+		if(opt.isPresent()) {
+			return opt.get();
+		}else
+		{
+			return null;
+		}
+	}
+
+	@Override
+	public Application save(Application todo) {
+		return repo.save(todo);
+	}
+
+	@Override
+	public void remove(Application todo) {
+		repo.delete(todo);
+	}
+}
